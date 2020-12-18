f30e89e94d9730b62d29abee96a453f97e92d249
kiran94 <kiran_patel94@hotmail.com>
2020-12-18 23:53:52 +0000

    implemented syncing configuration

--- dotfiles/subcommands/sync/__init__.py
+++ dotfiles/subcommands/sync/__init__.py
@@ -2,14 +2,31 @@
 from argparse import ArgumentParser, Namespace
 import logging
 from typing import Dict
+from shutil import copyfile
+from distutils.dir_util import copy_tree
+
+from rich.progress import track
+
+from dotfiles.core.matcher import match, ConfigurationFileType
 
 COMMAND = "sync"
 
 logger = logging.getLogger(__name__)
 
 def add_sub_parser(parser: ArgumentParser):
-    sub = parser.add_parser(COMMAND)
+    _ = parser.add_parser(COMMAND)
 
 def run(config: Dict, args: Namespace):
-    print('running sync')
-    pass
+
+    config_dir = args.config_dir
+    matches = list(match(config, config_dir, hide_progress=True))
+
+    for config in track(matches, description=f'Syncing Configuration...'):
+
+        if config.source_type == ConfigurationFileType.FILE:
+            logger.info('Copying File %s => %s', config.source, config.target)
+            copyfile(config.source, config.target)
+
+        elif config.source_type == ConfigurationFileType.DIRECTORY:
+            logger.info('Copying Directory %s => %s', config.source, config.target)
+            copy_tree(config.source, config.target)
