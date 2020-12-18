feb5cc83c299317870bd0ff31fd64f1139b69fec
kiran94 <kiran_patel94@hotmail.com>
2020-12-18 23:17:45 +0000

    implemented file update

---
+++ dotfiles/subcommands/update/__init__.py
@@ -0,0 +1,30 @@
+
+from argparse import ArgumentParser, Namespace
+import logging
+from typing import Dict
+from shutil import copy, copyfile
+
+from rich.progress import track
+
+from dotfiles.core.matcher import match, ConfigurationFileType
+
+COMMAND = "update"
+
+logger = logging.getLogger(__name__)
+
+def add_sub_parser(parser: ArgumentParser):
+    _ = parser.add_parser(COMMAND)
+
+def run(config: Dict, args: Namespace):
+
+    config_dir = args.config_dir
+    matches = list(match(config, config_dir, hide_progress=True))
+
+    for config in track(matches, description=f'Updating Configuration Directory: {config_dir}'):
+
+        if config.source_type == ConfigurationFileType.FILE:
+            logger.info('Copying %s => %s', config.target, config.source)
+            copyfile(config.target, config.source)
+
+        elif config.source == ConfigurationFileType.DIRECTORY:
+            pass
