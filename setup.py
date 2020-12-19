80b301263c4ef2fc36527fc76fd0d1aa93365e34
kiran94 <kiran_patel94@hotmail.com>
2020-12-19 17:03:53 +0000

    build: ensure package dependencies are declared in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import re
 from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+    requirements = requirements.split('\n')
 
 setup(
     name='dotfiles',
     version='0.0.1',
     packages = ['dotfiles'],
+    install_requires = requirements,
     entry_points = {
         'console_scripts': [
             'dotfiles = dotfiles.__main__:main'
